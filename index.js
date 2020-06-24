4a2160fcdac38c7a0766015d377334f40d5a85ec
Harrison Aguiar <harryaguiar@gmail.com>
2020-06-23 21:35:26 -0400

    task 6 done

--- index.js
+++ index.js
@@ -81,11 +81,11 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(removeFlavor){
+function removeLastFlavor(originalFlavors){
 
     let lastItem = originalFlavors.pop()
 
-    return removeFlavor
+    return originalFlavors;
 }
 
 console.log(removeLastFlavor(originalFlavors));
@@ -99,15 +99,15 @@ Your function should accept:
 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
-function getFlavorByIndex(findFlavor){
+function getFlavorByIndex(originalFlavors, a){
 
-    let flavor = originalFlavors;
+    // let flavor = originalFlavors;
 
-    return findFlavor;
+    return originalFlavors[a];
 
 }
 
-console.log(getFlavorByIndex(originalFlavors[6]));
+console.log(getFlavorByIndex(originalFlavors, 6));
 
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
 
@@ -122,15 +122,17 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(removeFlavor){
+// console.log(originalFlavors[24]);
 
-    let flavor = originalFlavors.splice(24, 1);
+// function removeFlavorByName(removeFlavor){
 
-    return removeFlavor;
+//     let flavor = originalFlavors.splice(24, 1);
 
-}
+//     return removeFlavor;
 
-console.log(removeFlavorByName(originalFlavors));
+// }
+
+// console.log(removeFlavorByName(originalFlavors));
 
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
@@ -141,11 +143,16 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
+function copy(copyOriginalFlavors){
 
-    /*code here*/
+    let copyFlavors = [...originalFlavors];
+
+   return copyFlavors;
 
 }
+
+console.log(copy(originalFlavors));
+
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -162,7 +169,7 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
+function filterByWord(originalFlavors){
 
     /*code here*/
 
